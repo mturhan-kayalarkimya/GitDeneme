e515bd495895478e093cb8c889f0d98d042633c6
mturhan <mturhan@kayalarkimya.com.tr>
2023-09-12 14:11:59 +0300

    Branch mturhan değişikliği eklendi.

--- GitDeneme/Program.cs
+++ GitDeneme/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Değişiklik 1");
+Console.WriteLine("Hello, World! Değişiklik 1 DenemeDegisiklik_mtruhan");
